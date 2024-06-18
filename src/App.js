bbe38e0cf4ff16dfce6605e7a9481e3754465aea
Sello Mathobela <Sellommathobela39@gmail.com>
2024-06-18 21:52:11 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import MainApp from './MainApp'
 
 function App() {
+  // const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> <MainApp /> </div>
   );
 }
 
